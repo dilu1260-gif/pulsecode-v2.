051e34f6a49c8553e8944c1ca9bcba116ffc14ef
Dilawar <dilu1260@gmail.com>
2026-07-05 16:35:04 +0000

    feat(editor): add navigation state for jump-to-line

--- src/components/editor/editorStore.ts
+++ src/components/editor/editorStore.ts
@@ -4,16 +4,34 @@ import type * as monaco from "monaco-editor";
 interface EditorStore {
   editor: monaco.editor.IStandaloneCodeEditor | null;
 
+  targetLine: number | null;
+
   setEditor: (
     editor: monaco.editor.IStandaloneCodeEditor | null
   ) => void;
+
+  jumpToLine: (line: number) => void;
+
+  clearJump: () => void;
 }
 
 export const useEditorStore = create<EditorStore>((set) => ({
   editor: null,
 
+  targetLine: null,
+
   setEditor: (editor) =>
     set({
       editor,
+    }),
+
+  jumpToLine: (line) =>
+    set({
+      targetLine: line,
+    }),
+
+  clearJump: () =>
+    set({
+      targetLine: null,
     }),
 }));
